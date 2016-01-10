b617048850a696527b800d077c10058d2952f324
yuanyue <yuanyue@wustl.edu>
2016-01-10 15:56:53 -0600

    Initial Commit

---
+++ MoviePlazaTests/MoviePlazaTests.swift
@@ -0,0 +1,36 @@
+//
+//  MoviePlazaTests.swift
+//  MoviePlazaTests
+//
+//  Created by Lily on 1/10/16.
+//  Copyright © 2016 yyclaire. All rights reserved.
+//
+
+import XCTest
+@testable import MoviePlaza
+
+class MoviePlazaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
